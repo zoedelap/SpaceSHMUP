8949a4a1e1cc26e1ff6adcaafd45f24fdb7e19ff
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-11-07 11:43:50 -0600

    Basic Hero movement

--- SpaceSHMUP/Assets/__Scripts/Hero.cs
+++ SpaceSHMUP/Assets/__Scripts/Hero.cs
@@ -1,18 +1,41 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    static public Hero S { get; private set; }
+
+    [Header("Inscribed")]
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    [Header("Dynamic")]
+    [Range(0, 4)]
+    public float shieldLevel = 1;
+
+    void Awake()
     {
-        
+        if (S == null)
+        {
+            S = this;
+        } else
+        {
+            print("Hero.Awake(0 - Attempted to assign second Hero.S!");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+
+        pos.x += hAxis * speed * Time.deltaTime;
+        pos.y += vAxis * speed * Time.deltaTime;
+        transform.position = pos;
     }
 }
