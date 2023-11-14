f077a8e9321234344b1003b52c03292a6dfca32c
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-11-14 12:06:02 -0600

    spawn enemies

---
+++ SpaceSHMUP/Assets/__Scripts/Main.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    static private Main S;
+
+    [Header("Inscribed")]
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemyInsetDefault = 1.5f;
+
+    private BoundsCheck bndCheck;
+
+    private void Awake()
+    {
+        S = this; 
+        bndCheck = GetComponent<BoundsCheck>();
+        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+    }
+
+    public void SpawnEnemy()
+    {
+        int ndx = Random.Range(0, prefabEnemies.Length);
+        GameObject go = Instantiate(prefabEnemies[ndx]);
+
+        float enemyInset = enemyInsetDefault;
+        if (go.GetComponent<BoundsCheck>() != null)
+        {
+            enemyInset = Mathf.Abs(go.GetComponent<BoundsCheck>().radius);
+        }
+
+        Vector3 pos = Vector3.zero;
+        float xMin = -bndCheck.camHeight + enemyInset;
+        float xMax  = bndCheck.camWidth - enemyInset;
+        pos.x = Random.Range(xMin, xMax);
+        pos.y = bndCheck.camHeight + enemyInset;
+        go.transform.position = pos;
+
+        Invoke(nameof(SpawnEnemy), 1f / enemySpawnPerSecond);
+    }
+}
