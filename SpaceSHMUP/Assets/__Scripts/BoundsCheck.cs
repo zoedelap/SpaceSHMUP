e90fc1b700d73c53425011c6b1215dc442a23e86
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-11-14 11:39:31 -0600

    added enemy and fixed shield

--- SpaceSHMUP/Assets/__Scripts/BoundsCheck.cs
+++ SpaceSHMUP/Assets/__Scripts/BoundsCheck.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class BoundsCheck : MonoBehaviour
 {
+    [System.Flags]
+    public enum eScreenLocs
+    {
+        onScreen = 0,   // 0000
+        offRight = 1,   // 0001
+        offLeft = 2,    // 0010
+        offUp = 4,      // 0100
+        offDown = 8     // 1000
+    }
     public enum eType {  center, inset, outset };
 
     [Header("Inscribed")]
     public eType boundsType = eType.center;
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Dynamic")]
+    public eScreenLocs screenLocs = eScreenLocs.onScreen;
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
 
@@ -28,24 +40,32 @@ public class BoundsCheck : MonoBehaviour
         if (boundsType == eType.outset) checkRadius = radius;
 
         Vector3 pos = transform.position;
-        
+        isOnScreen = true;
+
         if (pos.x > camWidth + checkRadius)
         {
             pos.x = camWidth + checkRadius;
+            isOnScreen = false;
         }
         if (pos.x < -camWidth - checkRadius)
         {
             pos.x = -camWidth - checkRadius;
+            isOnScreen = false;
         }
 
         if (pos.y > camHeight + checkRadius) {
             pos.y = camHeight + checkRadius;
+            isOnScreen = false;
         }
         if (pos.y < -camHeight - checkRadius)
         {
             pos.y = -camHeight - checkRadius;
+            isOnScreen = false;
         }
 
-        transform.position = pos;
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+        }
     }
 }
---
+++ SpaceSHMUP/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoundsCheck))]
+public class Enemy : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public float speed = 10f;
+    public float fireRate = 0.3f;
+    public float health = 10;
+    public int score = 100;
+
+    private BoundsCheck bndCheck;
+
+    public Vector3 pos
+    {
+        get
+        {
+            return this.transform.position;
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+
+    private void Awake()
+    {
+        bndCheck = GetComponent<BoundsCheck>();    
+    }
+
+    void Update()
+    {
+        Move();
+
+        if (!bndCheck.isOnScreen)
+        {
+            if (pos.y < bndCheck.camHeight - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    public virtual void Move()
+    {
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+}
