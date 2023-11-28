8a7bda79105549eda595db11f0cd163a8cb270ce
Zoe Delap <73131522+zoedelap@users.noreply.github.com>
2023-11-28 12:27:04 -0600

    shooting and hero pitch and roll

--- SpaceSHMUP/Assets/__Scripts/Hero.cs
+++ SpaceSHMUP/Assets/__Scripts/Hero.cs
@@ -12,6 +12,8 @@ public class Hero : MonoBehaviour
     public float speed = 30;
     public float rollMult = -45;
     public float pitchMult = 30;
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 40;
 
     [Header("Dynamic")] [Range(0, 4)] [SerializeField]
     private float _shieldLevel = 1;
@@ -39,6 +41,22 @@ public class Hero : MonoBehaviour
         pos.x += hAxis * speed * Time.deltaTime;
         pos.y += vAxis * speed * Time.deltaTime;
         transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(vAxis * pitchMult, hAxis * rollMult, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TempFire();
+        }
+    }
+
+    void TempFire()
+    {
+        GameObject projGO = Instantiate<GameObject>(projectilePrefab);
+        projGO.transform.position = transform.position;
+
+        Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+        rigidB.velocity = Vector3.up * projectileSpeed;
     }
 
     private void OnTriggerEnter(Collider other)
